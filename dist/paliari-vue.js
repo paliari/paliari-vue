9f22daef613911bb13d193831132e6e0cf8e7af9
Marcos Paliari <marcos@paliari.com.br>
2017-02-11 13:03:06 -0200

    yarn release

--- dist/paliari-vue.js
+++ dist/paliari-vue.js
@@ -250,14 +250,34 @@ function setLocale(locales) {
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return deepKey; });
 function deepKey(obj, key) {
-  if (obj && key) {
-    for (var k in String(key).split('.')) {
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = String(key).split('.')[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var k = _step.value;
+
       if (!obj) {
         return obj;
       }
       obj = obj[k];
     }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
   }
+
   return obj;
 }
 

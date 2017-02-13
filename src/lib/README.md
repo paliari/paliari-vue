ca52ce9f17117b8c9dc3d1f0ce63c91a4cfe77e9
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-02-13 15:49:49 -0200

    add prompt dialog

--- src/lib/README.md
+++ src/lib/README.md
@@ -8,7 +8,11 @@ Usage in component:
 ```javascript
 this.$dialogs.alert('Alterado com sucesso').then(() => {/* Do something heer */})
 
-this.$dialogs.confirm('Tem certeza que deseja sair?', 'Confirmação')
+this.$dialogs.confirm('Tem certeza que deseja sair?', 'Confirmação', 'NÃO', 'SIM')
 .then(() => { console.log('confirmed') })
+.catch(() => { console.log('canceled') })
+
+this.$dialogs.prompt('Tem certeza que deseja sair?', 'Confirmação')
+.then((input) => { console.log('Input: ', input) })
 .catch(() => { console.log('canceled') })
 ```
--- src/lib/dialogs.js
+++ src/lib/dialogs.js
@@ -9,6 +9,7 @@ export default {
       }
     })
   },
+
   confirm (message, title = '', cancelLabel = 'Cancelar', okLabel = 'OK') {
     return new Promise((resolve, reject) => {
       if (navigator.notification) {
@@ -20,6 +21,22 @@ export default {
         )
       } else {
         window.confirm(message) ? resolve() : reject()
+      }
+    })
+  },
+
+  prompt (message, title = '', cancelLabel = 'Cancelar', okLabel = 'OK') {
+    return new Promise((resolve, reject) => {
+      if (navigator.notification) {
+        navigator.notification.prompt(
+          message,
+          (response) => { response.buttonIndex === 2 ? resolve(response.input1) : reject() },
+          title,
+          [cancelLabel, okLabel]
+        )
+      } else {
+        ret = window.prompt(message)
+        ret ? resolve(ret) : reject()
       }
     })
   }

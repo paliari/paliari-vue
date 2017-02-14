8cc26a52d1f6fe1e8369264995e6f3e86f9d9014
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-02-14 09:23:25 -0200

    fix dialogs

--- src/lib/dialogs.js
+++ src/lib/dialogs.js
@@ -1,42 +1,42 @@
 export default {
   alert (message, title = '', buttonLabel = '') {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       if (navigator.notification) {
-        navigator.notification.alert(message, resolve, title, buttonLabel)
+        navigator.notification.alert(message, () => { resolve(true) }, title, buttonLabel)
       } else {
         window.alert(message)
-        resolve()
+        resolve(true)
       }
     })
   },
 
   confirm (message, title = '', cancelLabel = 'Cancelar', okLabel = 'OK') {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       if (navigator.notification) {
         navigator.notification.confirm(
           message,
-          (buttonIndex) => { buttonIndex === 2 ? resolve() : reject() },
+          (buttonIndex) => { buttonIndex === 2 ? resolve(true) : resolve(false) },
           title,
           [cancelLabel, okLabel]
         )
       } else {
-        window.confirm(message) ? resolve() : reject()
+        window.confirm(message) ? resolve(true) : resolve(false)
       }
     })
   },
 
   prompt (message, title = '', cancelLabel = 'Cancelar', okLabel = 'OK') {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       if (navigator.notification) {
         navigator.notification.prompt(
           message,
-          (response) => { response.buttonIndex === 2 ? resolve(response.input1) : reject() },
+          (response) => { response.buttonIndex === 2 ? resolve(response.input1) : resolve(false) },
           title,
           [cancelLabel, okLabel]
         )
       } else {
         ret = window.prompt(message)
-        ret ? resolve(ret) : reject()
+        ret ? resolve(ret) : resolve(false)
       }
     })
   }

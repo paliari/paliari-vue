1ca4ab2d5865d79ce437c5fbbdc5a89c406aec5a
Marcos Paliari <marcos@paliari.com.br>
2017-02-28 10:14:22 -0300

    add lib/auth/Auth

---
+++ src/lib/auth/Auth.js
@@ -0,0 +1,51 @@
+const PATHS = {
+  signIn: '/sign_in',
+  signUp: '',
+  signOut: '/sign_out',
+  resetPassword: '/password',
+  current: '/current'
+}
+
+class Auth {
+  constructor({ api, TokenManager, basePath = '/auth', paths = {} }) {
+    this.api = api
+    this.TokenManager = TokenManager
+    this.paths = {}
+    for (let k in PATHS) {
+      this.paths[k] = basePath+(paths[k] || PATHS[k])
+    }
+	}
+
+  signIn (params) {
+    let promise = this.api.post(this.paths.signIn, params)
+    promise.then((response) => {
+      this.TokenManager.setToken(response.headers)
+    })
+    return promise
+  }
+
+  signUp (params) {
+    return this.api.post(this.paths.signUp, params)
+  }
+
+  resetPassword (params) {
+    return this.api.post(this.paths.resetPassword, params)
+  }
+
+  signOut (params) {
+    promise = this.api.delete(this.paths.signOut, params)
+    promise.then(() => { this.TokenManager.removeToken() })
+    return promise
+  }
+
+  getToken () {
+    return this.TokenManager.getToken()
+  }
+
+  current () {
+    return this.api.get(this.paths.current)
+  }
+
+}
+
+export default Auth

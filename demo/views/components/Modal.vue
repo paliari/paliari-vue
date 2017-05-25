cdccf2dba7ad27b2b270fd1a559538cc6851f91d
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-05-25 11:25:43 -0300

    update demo modal

--- demo/views/components/Modal.vue
+++ demo/views/components/Modal.vue
@@ -5,12 +5,20 @@
   <ul>
     <li><b>title</b> - The modal title</li>
     <li><b>show</b> - A required boolean to show/hide the modal</li>
-    <li><b>cancelLabe</b> - Label for the cancel button</li>
-    <li><b>okLabel</b> - Label for the ok button</li>
-    <li><b>permitSuccess</b> - If true the ok button will be disasbled</li>
+    <li><b>cancel-label</b> - Label for the cancel button</li>
+    <li><b>ok-label</b> - Label for the ok button</li>
+    <li><b>permit-success</b> - If true the OK button will be disasbled</li>
+    <li><b>header-background-color</b> - Background color for the modal header</li>
+    <li><b>header-color</b> - Text color for the modal header</li>
+  </ul>
+  <p>Events</p>
+  <ul>
+    <li><b>cancel</b> - Emited when click on the CANCEL button</li>
+    <li><b>success</b> - Emited when click on the OK button</li>
+    <li><b>close</b> - Emited when click on CANCEL or OK</li>
   </ul>
   Usage example
-  <pre>&lt;modal :show="showModal"&gt;&lt;/modal&gt;</pre>
+  <pre>&lt;modal :show="showModal" @close="showModal = false"&gt;&lt;/modal&gt;</pre>
   <button type="button" @click="showModal = true">Show Modal</button>
   <modal :show="showModal" @close="showModal = false" title="Modal example">
     <h1>Modal html content</h1>
@@ -29,13 +37,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus">
-.modal-demo
-  .modal-header
-    background-color #D1AF81
-    height 40px
-    padding 5px
-  .modal-body
-    padding 20px
-</style>

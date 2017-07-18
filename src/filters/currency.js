952cfd5c566710c119c4161ca50d3e1201383b6b
Marcos Paliari <marcos@paliari.com.br>
2017-07-18 17:29:38 -0300

    add sulfi in currency

--- src/filters/currency.js
+++ src/filters/currency.js
@@ -1,6 +1,6 @@
-export default (number, prefix = 'R$') => {
+export default (number, prefix = '', sufix = '') => {
   try {
-    return prefix + parseFloat(number).toFixed(2).replace(/\./g, ',').replace(/(\d)(?=(\d{3})+\,)/g, '$1.')
+    return prefix + parseFloat(number).toFixed(2).replace(/\./g, ',').replace(/(\d)(?=(\d{3})+\,)/g, '$1.') + sufix
   } catch (e) {
     return number
   }

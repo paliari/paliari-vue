f98610fa2739487c29441670cae8101325b74b73
Daniel Fernando Lourusso <dflourusso@gmail.com>
2017-10-07 11:41:30 -0300

    fix docs about how to cofigure i18n locales

--- demo/views/utils/I18n.vue
+++ demo/views/utils/I18n.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
   I18n by javascript
+  First you need to configure the locale file
+  <pre>import PaliariVue from 'paliari-vue'
+Vue.use(PaliariVue, {locales: require('./path/to/locales/en|pt-BR.json')})</pre>
   <pre>this.$util.i18n.hum('hello')</pre>
   <i>Output -></i> Hello world
 

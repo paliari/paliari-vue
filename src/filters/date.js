aa0f90c969eaafe935aaa7c937c85d183c3a12b1
Marcos Paliari <marcos@paliari.com.br>
2018-03-02 15:38:34 -0300

    fix Fecha parse date

--- src/filters/date.js
+++ src/filters/date.js
@@ -2,7 +2,7 @@ import Fecha from '../lib/fecha'
 
 export default (date, format = 'shortDateTime') => {
   try {
-    date = Fecha.parse(date, 'json') || date
+    date = Fecha.parse(date, 'YYYY-MM-DDTHH:mm:ssZZ') || date
     return Fecha.format(date, format)
   } catch (e) {
     return date
